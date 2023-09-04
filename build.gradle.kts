0109bd6d6ec865797bf11e28176905eb6de27f92
Dibyendu-Merlock <107022870+Dibyendu-Merlock@users.noreply.github.com>
2023-09-04 20:50:28 +0530

    added db info and env file and created movies class
    1. added db info in application.properties
    2. created .env file so that our db info doesn't get exposed to the web.
    3. since spring doesn't support reading of env file out the box that's why we use spring dotenv annotaion.
    4. created movies class, and annotate it as @Document(collection="movies"), since it will mark the class as a candidate for mapping to the database.

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,12 +22,16 @@ repositories {
 }
 
 dependencies {
-	//implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	// https://mvnrepository.com/artifact/me.paulschwarz/spring-dotenv
+	implementation("me.paulschwarz:spring-dotenv:2.5.4")//since spring doesn't have the abbility to read .env file out of the box. so we use this dependency
+
+
 }
 
 tasks.withType<Test> {
